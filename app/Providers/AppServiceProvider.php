25e3e35265ba95732a44e2203fb7cb01cf7964ca
ibnu099 <ibnu099@gmail.com>
2019-12-25 22:02:12 +0700

    Prevention key was to long error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Mengatasi key error untuk laravel < 5.7.7
+        Schema::defaultStringLength(191);
+
     }
 
     /**
