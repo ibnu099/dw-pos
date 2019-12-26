552a61458c0c6c74cb67e8b96a11afc29843753b
ibnu099 <ibnu099@gmail.com>
2019-12-26 21:14:18 +0700

    Managemen Kategori - Create layout Master

---
+++ resources/views/layouts/module/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2019-2023 <a href="http://adminlte.io">AdminLTE.io</a></strong>
+    All right reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.0.0-alpha
+    </div>
+</footer>
